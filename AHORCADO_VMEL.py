aa5499dd6c80c6d371b8f0c0cd4effcc4d8cdbd0
ErikaLizbeth74 <erika.vinocunga00@outlook.es>
2024-06-22 22:10:30 -0500

    Update AHORCADO_VMEL.py
    Bueno compañeros les presento aqui mi codigo del juego del ahorcado, al momento no esta culminado pero mantiene la funcion principal que es adivinar una palabra antes de que perder todas las oportunidades que son limitadas.
    
    El codigo funciona basica que es ingresar una letra el codigo ve si esta es correcta o incorrecta si es acertada se mostrara en la linea y si no es pues restara una oportunidad hasta quedar en cero también podemos ingresar la palabra completa si es acertada se ganara inmediata mente y si no se seguirá pidiendo o mas letras.
    
    Con el dibujo del ahorcado aun esta en proceso de creación.
    
    Espero que lo puedan entender y eso seria Todo. Gracias.

--- AHORCADO_VMEL.py
+++ AHORCADO_VMEL.py
@@ -1,3 +1,5 @@
+import random
+
 print('\n\n\n\n')
 print('=' * 75)
 print('                   BIENVENIDO AL JUEGO DEL AHORCADO     ')
@@ -25,10 +27,41 @@ print('=' * 75)
 nombre = input('               Introduce tu nombre : ')
 print('=' * 75)
 print('\n\n')
+print('=' * 75)
 print("                 Hola",nombre, "comencemos con el juego")
+print('=' * 75)
 print('\n')
+print('=' * 75)
+abecedario = print('                                 ABECEDARIO')
+print('\n')
+print ('A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z')
+print('=' * 75)
 
-import random
+def inicio():
+    print("   ▄████████    ▄█    █▄     ▄██████▄     ▄████████  ▄████████    ▄████████ ████████▄   ▄██████▄")
+    print("  ███    ███   ███    ███   ███    ███   ███    ███ ███    ███   ███    ███ ███   ▀███ ███    ███")
+    print("  ███    ███   ███    ███   ███    ███   ███    ███ ███    █▀    ███    ███ ███    ███ ███    ███")
+    print("  ███    ███  ▄███▄▄▄▄███▄▄ ███    ███  ▄███▄▄▄▄██▀ ███          ███    ███ ███    ███ ███    ███")
+    print("▀███████████ ▀▀███▀▀▀▀███▀  ███    ███ ▀▀███████▀   ███        ▀███████████ ███    ███ ███    ███")
+    print("  ███    ███   ███    ███   ███    ███ ▀███████████ ███    █▄    ███    ███ ███    ███ ███    ███")
+    print("  ███    ███   ███    ███   ███    ███   ███    ███ ███    ███   ███    ███ ███   ▄███ ███    ███")
+    print("  ███    █▀    ███    █▀     ▀██████▀    ███    ███ ████████▀    ███    █▀  ████████▀   ▀██████▀")
+
+
+def congratulation():
+    print("███████╗███████╗██╗     ██╗ ██████╗██╗██████╗  █████╗ ██████╗ ███████╗███████╗")
+    print("██╔════╝██╔════╝██║     ██║██╔════╝██║██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝")
+    print("█████╗  █████╗  ██║     ██║██║     ██║██║  ██║███████║██║  ██║█████╗  ███████╗")
+    print("██╔══╝  ██╔══╝  ██║     ██║██║     ██║██║  ██║██╔══██║██║  ██║██╔══╝  ╚════██║")
+    print("██║     ███████╗███████╗██║╚██████╗██║██████╔╝██║  ██║██████╔╝███████╗███████║")
+    print("╚═╝     ╚══════╝╚══════╝╚═╝ ╚═════╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝")
+
+
+
+
+print('\n')
+inicio()
+print('\n')
 
 escenario = \
     '''   
@@ -44,45 +77,64 @@ escenario = \
 
 simbolos = '|💀🦺🫱🫲🦴'
 
-escena = escenario
-for i in range(0, len(simbolos)):
-    simbolo = simbolos[i]
-    escena = escena.replace(str(i), simbolo)
-print(escena)
+def mostrar_escenario(errores):
+    escena = escenario
+    for i in range(0, len (simbolos)):
+        simbolo = simbolos [i]
+        escena = escena.replace(str(i), simbolo)
+    print(escena)
 
-## PARTE  DOS DEL TRABAJO
-## PARTE  DOS DEL TRABAJO
-## PARTE  DOS DEL TRABAJO
-## PARTE  DOS DEL TRABAJO
-## PARTE  DOS DEL TRABAJO
-
-palabras = ("leon", "tigre", "delfin", "manzana", "perro", "mandarina", "sandia")
+palabras = ("leon", "tigre")
 palabra = random.choice(palabras)
 letra_adivinadas=[]
+lista_abecedario = list('abcdefghijklmnñopqrstuvwxyz')
+intentos = 6
 
-while True:
 
-    text = input("Ingrese una letra del abecedario o la palabra completa: ").lower()
-    letra_adivinadas.append(text)
-    
+print(f'Tienes {intentos} intentos antes de perden BUENA SUERTE!')
+print(simbolos[6 - intentos])
+print('\n')
+
+while intentos>0:
+
     palabra_oculta = " "
     for c in palabra:
         if c in letra_adivinadas:
             palabra_oculta+=c
+            intentos>1
         else:
             palabra_oculta+= "_"
     print(palabra_oculta)
 
-    if "_" not in palabra_oculta:
-        print("Felicidades has Ganado la palabra es",(palabra))
-        break
+    print('\n')
+    text = input("Ingrese una letra del abecedario o la palabra completa: ").lower()
+    print('\n')
+    
+    if text in letra_adivinadas:
+        print("Ya has introducido esa letra. Prueba con otra.")
+        continue
+
+    if text in palabra:
+        letra_adivinadas.append(text)
+        if set(letra_adivinadas)==set(palabra):
+            congratulation()
+            print('\n')
+            print("Felicidades has Ganado la palabra es",(palabra))
+            break
+    else:
+        intentos-=1
+        print('\n')
+        print(f"Letra incorrecta, Te qudan",(intentos),"oportunidades")
+        print('\n')
+
+    if intentos==0:
+        print(f"Lo sineto has perdido, la palabra era",(palabra))
 
     elif len(text) == len(palabra):
         if text == palabra:
-            print("Has ganado la palabra es",(palabra))
+            congratulation()
+            print('\n')
+            print("Has ganado la palabra completa es",(palabra))
+            print('\n')
             break
-    
-
-
-
-print('\n\n')
+print('\n')
